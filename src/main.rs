db4700e5eb910c9cdeaa87f7a0e0cf97c263b4ee
Will Zegers <will.zegers@gmail.com>
2023-01-11 16:40:46 -0800

    Add camera and background coloring

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,33 @@ use std::fs;
 mod color;
 use crate::color::get_pixel_color;
 mod ray;
+use crate::ray::Ray;
 mod vec3;
 use crate::vec3::Vec3;
 
+fn ray_color(r: &Ray) -> Vec3 {
+    let unit_direction = r.direction.as_unit_vector();
+    let t = 0.5 * (unit_direction.y + 1.0);
+    (1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5, 0.7, 1.0)
+}
+
 fn main() {
     // Image
-    const IMAGE_WIDTH: u32 = 256;
-    const IMAGE_HEIGHT: u32 = IMAGE_WIDTH;
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
+    const IMAGE_WIDTH: u32 = 400;
+    const IMAGE_HEIGHT: u32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as u32;
     const OUTPUT_FILE: &str = "image.ppm";
+
+    //Camera
+    const VIEWPORT_HEIGHT: f64 = 2.0;
+    const VIEWPORT_WIDTH: f64 = ASPECT_RATIO * VIEWPORT_HEIGHT;
+    const FOCAL_LENGTH: f64 = 1.0;
+
+    let origin: Vec3 = Vec3::new(0.0, 0.0, 0.0);
+    let horizontal: Vec3 = Vec3::new(VIEWPORT_WIDTH, 0.0, 0.0);
+    let vertical: Vec3 = Vec3::new(0.0, VIEWPORT_HEIGHT, 0.0);
+    let lower_left_corner: Vec3 =
+        origin - (horizontal / 2.0) - (vertical / 2.0) - Vec3::new(0.0, 0.0, FOCAL_LENGTH);
 
     // Render
     let mut image = format!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
@@ -19,13 +38,13 @@ fn main() {
     for j in (0..IMAGE_HEIGHT).rev() {
         print!("\rScanlines remaining: {}", j);
         for i in 0..IMAGE_WIDTH {
-            let pixel = Vec3::new(
-                (i as f64) / ((IMAGE_WIDTH as f64) - 1.),
-                (j as f64) / ((IMAGE_HEIGHT as f64) - 1.),
-                0.25,
-            );
+            let u = (i as f64) / ((IMAGE_WIDTH - 1) as f64);
+            let v = (j as f64) / ((IMAGE_HEIGHT - 1) as f64);
 
-            let pixel_color = get_pixel_color(pixel);
+            let direction = lower_left_corner + (u * horizontal) + (v * vertical) - origin;
+            let r = Ray::new(origin, direction);
+            let pixel_color = get_pixel_color(ray_color(&r));
+
             write!(image, "{pixel_color}").unwrap();
         }
     }
